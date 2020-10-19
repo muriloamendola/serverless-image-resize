80ebe1a144c55ccf5e2f994364428cdd6ed3b8ea
muriloamendola <muriloamendola@gmail.com>
2020-10-18 23:20:05 -0300

    First commit

---
+++ serverless.ts
@@ -0,0 +1,50 @@
+/* eslint-disable no-template-curly-in-string */
+import type { Serverless } from 'serverless/aws'
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: 'serverless-image-resize'
+  },
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  // Add the serverless-webpack plugin
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    region: 'us-east-1',
+    stage: 'dev',
+    memorySize: 512, // Default is 1024
+    apiGateway: {
+      minimumCompressionSize: 1024
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1'
+    },
+    deploymentBucket: {
+      name: 'serverless-image-resize.${self:provider.stage}.deploys',
+      blockPublicAccess: true
+    },
+    versionFunctions: false
+  },
+  functions: {
+    hello: {
+      handler: 'handler.hello',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'hello'
+          }
+        }
+      ]
+    }
+  }
+}
+
+module.exports = serverlessConfiguration
