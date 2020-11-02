2a34114c7b6a9df41ac1b93960ab0413e16ea1e6
muriloamendola <muriloamendola@gmail.com>
2020-11-02 15:51:56 -0300

    Just instatiate S3 client in global scope

--- src/utils/s3Client.ts
+++ src/utils/s3Client.ts
@@ -1,9 +1,10 @@
 import * as AWS from 'aws-sdk'
+const S3 = new AWS.S3()
 
 export function getObject (params: {Bucket: string, Key: string}) {
-  return new AWS.S3().getObject(params).promise()
+  return S3.getObject(params).promise()
 }
 
 export function upload (params: {Bucket: string, Key: string, Body: Buffer, ContentType?: string}) {
-  return new AWS.S3().upload(params).promise()
+  return S3.upload(params).promise()
 }
